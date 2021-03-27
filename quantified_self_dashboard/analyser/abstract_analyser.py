9b98dcd3e12eb615585377ee8ad0021851e12813
janagoe <goedeke.jana@gmail.com>
2021-03-27 18:41:55 +0100

    Documenting and refactoring Analysers

--- quantified_self_dashboard/analyser/abstract_analyser.py
+++ quantified_self_dashboard/analyser/abstract_analyser.py
@@ -1,34 +1,99 @@
+import os
 from typing import List
 from summary.summary_container import SummaryContainer
-from common.constants import *
-import os
+from common.constants import AnalysisType, Periodicity
 
 
-class AbstractAnalyzer:
+class AbstractAnalyser:
+    """ 
+    Abstract Analyser class, providing wrapper methods for many different 
+    analyser types.
+    """
 
-    _supported_analysis_types = []
+    # Analysis Types supported by this analyser 
+    _supported_analysis_types: List[AnalysisType] = []
+
 
     def __init__(self, output_path: List[str], container: SummaryContainer):
+        """
+        Parameters
+        ----------
+        output_path : List[str]
+            Path where output images/files can be saved.
+
+        container : SummaryContainer
+            Where the analyser can get its analysis data from.
+        """
         self._container = container
         self._output_location = os.path.join(os.getcwd(), *output_path)
 
-    def __check_parameters(self, start_date, end_date, periodicity, analysis_type, *args):
-        # check start before end
 
-        # eck all dates inside of summary container
+    def analyse(self, start: str, end: str, periodicity: Periodicity, analysis_type: AnalysisType, *args):
+        """
+        Parameters
+        ----------
+        start : str
+            Analysis start date in YYYY-MM-DD format.
 
-        if not periodicity in [PERIODICITY_DAILY, PERIODICITY_WEEKLY, PERIODICITY_MONTHLY]:
-            raise ValueError("Invalid Analysis Periodicity Parameter")
+        end : str
+            Analysis end date in YYYY-MM-DD format.
+
+        periodicity : Periodicity
+            Which timeframes must be considered, e.g. daily, weekly, monthyl, yearly.
+
+        analysis_type : AnalysisType
+            The chosen type of anayzis for this method call.
+        """
+        self.__check_parameters(start, end, periodicity, analysis_type, *args)
+        self._analyse(start, end, periodicity, analysis_type, *args)
+    
+
+    def __check_parameters(self, start: str, end: str, periodicity: Periodicity, analysis_type: AnalysisType, *args):
+        """
+        Parameters
+        ----------
+        start : str
+            Analysis start date must be in YYYY-MM-DD format and before the end date.
+
+        end : str
+            Analysis end date must be in YYYY-MM-DD format and after the start date.
+
+        periodicity : Periodicity
+            Which timeframes must be considered, e.g. daily, weekly, monthyl, yearly.
+
+        analysis_type : AnalysisType
+            The chosen type of anayzis for this method call, must be part of the supported
+            analysis types of this Analyser Class.
+        """
 
         if not analysis_type in self._supported_analysis_types:
-            raise ValueError("Invalid Analysis Type Parameter")
+            raise ValueError("Invalid AnalysisType Parameter")
 
-    def _analyse(self, start_date: str, end_date: str, periodicity, analysis_type, *args):
+        # TODO: check if all dates are given in the summary container
+        # TODO: check if start is before end
+        # TODO: check if dates have valid format
+
+
+    def _analyse(self, start_date: str, end_date: str, periodicity: str, analysis_type, *args):
+        """
+        Parameters
+        ----------
+        start : str
+            Analysis start date in YYYY-MM-DD format.
+
+        end : str
+            Analysis end date in YYYY-MM-DD format.
+
+        periodicity : Periodicity
+            Which timeframes must be considered, e.g. daily, weekly, monthyl, yearly.
+
+        analysis_type : AnalysisType
+            The chosen type of anayzis for this method call, must be part of the supported
+            analysis types of this Analyser Class.
+        """
+
         # here each subclass implementation starts
         raise NotImplementedError("Abstract Class")
 
-    def analyze(self, start_date: str, end_date: str, periodicity, analysis_type, *args):
-        self.__check_parameters(start_date, end_date, periodicity, analysis_type, *args)
-        self._analyse(start_date, end_date, periodicity, analysis_type, *args)
 
 
---
+++ quantified_self_dashboard/analyser/plotly_analyser.py
@@ -0,0 +1,198 @@
+import os
+from typing import List, Tuple
+
+import numpy as np
+from collections import Counter
+import plotly.express as px
+
+from summary.summary_container import SummaryContainer
+from common.constants import *
+from common.date_helper import all_date_strings_between_dates
+from analyser.abstract_analyser import AbstractAnalyser
+from analyser.transform_measurements import *
+
+
+class PlotlyAnalyser(AbstractAnalyser):
+    """
+    Analysing SummaryContainer data by plotting, using the plotly library.
+    """
+
+
+    _supported_analysis_types = [AnalysisType.test]
+
+
+    def __init__(self, output_path: List[str], container: SummaryContainer):
+        """
+        Parameters
+        ----------
+        output_path : List[str]
+            Path where output images/files can be saved.
+
+        container : SummaryContainer
+            Where the analyser can get its analysis data from.
+        """
+        super().__init__(output_path, container)
+        
+
+    def _analyse(self, start: str, end: str, periodicity, analysis_type, *args):
+        """
+        Implementation of this analyser classes analysis.
+
+        Parameters
+        ----------
+        start : str
+            Analysis start date in YYYY-MM-DD format.
+
+        end : str
+            Analysis end date in YYYY-MM-DD format.
+
+        periodicity : Periodicity
+            Which timeframes must be considered, e.g. daily, weekly, monthyl, yearly.
+
+        analysis_type : AnalysisType
+            The chosen type of anayzis for this method call, must be part of the supported
+            analysis types of this Analyser Class.
+        """
+
+        # TODO: implementing switch-case-like structure for all different supported analysis types
+        self.__plot_multiple_of_same_unit(start, end, Periodicity.daily, [(SLEEP, "rmssd")])
+
+
+    def __plot_multiple_of_same_unit(self, start: str, end: str, periodicity: Periodicity, summary_type_measurement_tuples: List[Tuple[str, str]], **kwargs):
+        """
+        Plotting one or multiple lines in one plot. 
+        The different measurements should preferably have the same
+        unit to not screw the scale of the axis.
+
+        Parameters
+        ----------
+        start : str
+            Analysis start date in YYYY-MM-DD format.
+
+        end : str
+            Analysis end date in YYYY-MM-DD format.
+
+        periodicity : Periodicity
+            Which timeframes must be considered, e.g. daily, weekly, monthyl, yearly.
+
+        summary_type_measurement_tuples : List[Tuple[str, str]]
+            List of Tuples, where each tuple represents the summary type and 
+            measurement name which will get plotted.    
+
+        **kwargs:
+            Additional keyword arguments.
+            
+            title : str
+                Title of the plot.
+            
+            output_file_name : str
+                Name of the output file.
+        """
+
+        # x-axis
+        dates = all_date_strings_between_dates(start, end)
+
+        plot_data_sets, plot_legends, plot_units = [], [], []
+
+        # receiving all plot sets from the summary container as numpy arrays
+        for summary_type, measurement_name in summary_type_measurement_tuples:
+            plot_data, plot_legend_name, plot_unit = self.__get_plottable_data(start, end, periodicity, summary_type, measurement_name)
+
+            plot_data_sets.append(plot_data)
+            plot_legends.append(plot_legend_name)
+            plot_units.append(plot_unit)
+
+        # check if each plot data has same unit, otherwise the scale might get screwd
+        if len(Counter(plot_units).keys()) > 1:
+            raise ValueError("Different Units")
+
+        if 'title' in kwargs.keys():
+            title = kwargs['title']
+        else:
+            title = "Line Plot"
+
+        unit_as_text = UnitsAnnotationText[plot_units[0]]
+
+        # drawing plotly figure
+        fig = px.line(x=dates, y=plot_data_sets, title=title, labels={'x': 'Dates', 'y': unit_as_text})
+
+        # setting the legend to identify the different lines
+        for i, legend_name in enumerate(plot_legends):
+            fig['data'][i]['name'] = legend_name
+
+        # saving the plot as an image
+        if 'output_file_name' in kwargs.keys():
+            output_file_name = kwargs['output_file_name']
+        else:
+            output_file_name = "temp"
+        self.__save(fig, output_file_name)
+
+
+    def __get_plottable_data(self, start: str, end: str, periodicity: Periodicity, summary_type: str, measurement_name: str) -> Tuple[np.array, str, Unit]:
+        """
+        Retrieving the data form the summary container, 
+        transforming the data into a better format,
+        and getting
+
+        Parameters
+        ----------
+        start : str
+            Analysis start date in YYYY-MM-DD format.
+
+        end : str
+            Analysis end date in YYYY-MM-DD format.
+
+        periodicity : Periodicity
+            Which timeframes must be considered, e.g. daily, weekly, monthyl, yearly.
+
+        summary_type : str
+            Type of summary to plot the measurement name of.
+
+        measurement_name : str
+            Name of the measurement.
+
+        Returns
+        --------
+        Tuple
+            np.array of data to plot, 
+            title of name of the measurement,
+            unit of the measurement
+        """
+
+        if periodicity == Periodicity.daily:
+            raw_data = self._container.get_np_array(start, end, summary_type, measurement_name)
+        elif periodicity == Periodicity.weekly:
+            raise NotImplementedError()
+        elif periodicity == Periodicity.monthly:
+            raise NotImplementedError()
+        elif periodicity == Periodicity.yearly:
+            raise NotImplementedError()
+
+        # TODO: weekly, monthly and yearly averaging
+
+        # data, if no transformation is possible
+        plot_data = raw_data
+        measurement_title = "{} {}".format(summary_type, measurement_name)
+        trans_unit = Unit.undefined
+
+        # transforming the raw data if possible
+        # e.g. seconds into hours
+        summary_transform = summary_type_transform[summary_type]
+        if measurement_name in summary_transform.keys():
+
+            measurement_transform = summary_transform[measurement_name]
+            trans_func, _, _, _, trans_unit, measurement_title = measurement_transform
+
+            # applying the transformation function to the entire np.array
+            vfunc = np.vectorize(trans_func)
+            plot_data = vfunc(raw_data)
+
+        return plot_data, measurement_title, trans_unit
+
+
+    def __save(self, fig, file_name):
+        """ Saving the plotly figure as an image into the given output file name. """
+        path_name = "{}.png".format(os.path.join(self._output_location, file_name))
+        fig.write_image(path_name)
+
+
